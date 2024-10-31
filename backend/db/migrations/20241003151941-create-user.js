efff0b8f485f0fd9c0ed675ffdd5bbaa7bcdc560
Nia C <157457101+nlclark216@users.noreply.github.com>
2024-10-31 16:38:55 -0400

    Add User model scope for user security

---
+++ backend/db/migrations/20241003151941-create-user.js
@@ -0,0 +1,52 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+};
+
+options.tableName = "Users";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable({ tableName: 'Users', schema: options.schema }, {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      username: {
+        type: Sequelize.STRING(30),
+        allowNull: false,
+        unique: true
+      },
+      email: {
+        type: Sequelize.STRING(256),
+        allowNull: false,
+        unique: true
+      },
+      hashedPassword: {
+        type: Sequelize.STRING.BINARY,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      }
+    }, { tableName: 'Users', schema: options.schema });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable(options);
+  }
+};
+
+console.log('create-user', options.schema)
+console.log('create-user', options)
