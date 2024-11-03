bd0c6e5cdd600adb48fb8af47860b4465e7ed956
Nia C <157457101+nlclark216@users.noreply.github.com>
2024-11-02 20:08:11 -0400

    avgRating and previewImg working for get all spots

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 
 const { requireAuth } = require('../../utils/auth');
-const { Spot } = require('../../db/models');
+const { Spot, Sequelize, Review, SpotImage } = require('../../db/models');
 
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
@@ -11,13 +11,51 @@ const router = express.Router();
 
 // Get all spots
 router.get('/', async (req, res) => {
-    const spots = await Spot.findAll();
+    const spots = await Spot.findAll({
+        attributes: {
+            include: [
+              [Sequelize.fn('AVG', Sequelize.col('Reviews.stars')), 'avgRating'],
+              [Sequelize.col('SpotImages.url'), 'previewImage']
+      
+            ]
+          },
+        include: [
+        {
+            model: Review, 
+            attributes: []
+        },
+        {
+            model: SpotImage,
+            attributes: []
+        }
+        ],
+        group: ['Spot.id', 'SpotImages.url']
+    });
+
+    // Format the response
+    const formattedSpots = spots.map(spot => ({
+        id: spot.id,
+        ownerId: spot.ownerId,
+        address: spot.address,
+        city: spot.city,
+        state: spot.state,
+        country: spot.country,
+        lat: spot.lat,
+        lng: spot.lng,
+        name: spot.name,
+        description: spot.description,
+        price: spot.price,
+        createdAt: spot.createdAt,
+        updatedAt: spot.updatedAt,
+        avgRating: spot.get('avgRating') ? +parseFloat(spot.get('avgRating')).toFixed(1) : null,
+        previewImage: spot.get('previewImage') || null
+    }));
     
     return res.status(200).json({
-        Spots: spots
+        Spots: formattedSpots
     });
-    }  
-);
+  }  
+); 
 
 router.get('/current', requireAuth, async (req, res) => {
     const { user } = req;
