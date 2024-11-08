a60129f1fcf4256729ec31d8b22f8c167324290b
Nia C <157457101+nlclark216@users.noreply.github.com>
2024-11-08 10:37:36 -0500

    added try catch to add xsrf-token cookie

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,12 +3,12 @@ const express = require('express');
 const router = express.Router();
 
 // Add a XSRF-TOKEN cookie
-router.get("/api/csrf/restore", (req, res) => {
+router.get("/api/csrf/restore", (req, res, next) => {
     const csrfToken = req.csrfToken();
-    res.cookie("XSRF-TOKEN", csrfToken);
-    res.status(200).json({
-      'XSRF-Token': csrfToken
-    });
+    try {
+      res.cookie("XSRF-TOKEN", csrfToken);
+      res.status(200).json({ 'XSRF-Token': csrfToken });
+    } catch (error) { next(error); };
 });
 
 const apiRouter = require('./api');
