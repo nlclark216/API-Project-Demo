63226b6ecec0400727b1dd483dc533d18999e0eb
Nia C <157457101+nlclark216@users.noreply.github.com>
2024-11-10 23:53:20 -0500

    fixed existing review return status

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -502,7 +502,7 @@ router.post('/:spotId/reviews', restoreUser, requireAuth, validateReview, async 
     });
 
     if (existingReview) {
-      return res.status(403).json({ 
+      return res.status(500).json({ 
         message: "User already has a review for this spot" });
     };
 
