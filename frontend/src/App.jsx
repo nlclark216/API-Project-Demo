e99775e71e8b15eb3728bf9a9d512a77f4e87fd5
Nia C <157457101+nlclark216@users.noreply.github.com>
2024-11-21 18:01:03 -0500

    navigation working

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,49 @@
 // frontend/src/App.jsx
-
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { Outlet, createBrowserRouter, RouterProvider } from 'react-router-dom';
 import LoginFormPage from './components/LoginFormPage';
 import SignupFormPage from './components/SignupFormPage';
+import Navigation from './components/Navigation';
+import * as sessionActions from './store/session';
+
+function Layout() {
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    dispatch(sessionActions.restoreUser()).then(() => {
+      setIsLoaded(true)
+    });
+  }, [dispatch]);
+
+  return (
+    <>
+      <Navigation isLoaded={isLoaded}/>
+      {isLoaded && <Outlet />}
+    </>
+  );
+}
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <h1>Welcome!</h1>
-  },
-  {
-    path: '/login',
-    element: <LoginFormPage />
-  }, 
-  {
-    path: '/signup',
-    element: <SignupFormPage />
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <h1>Welcome!</h1>
+      },
+      {
+        path: '/login',
+        element: <LoginFormPage />
+      }, 
+      {
+        path: '/signup',
+        element: <SignupFormPage />
+      }
+    ]
   }
+  
 ]);
 
 function App() {
---
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -0,0 +1,10 @@
+import { CgProfile } from "react-icons/cg";
+
+
+export default function ProfileButton() {
+    return (
+        <div style={{width:'300px', height:'300px'}}>
+            <CgProfile />
+        </div>
+    );
+} 
