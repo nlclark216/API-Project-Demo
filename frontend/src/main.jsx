d257ff05b515dea5dd3c190a61c45baad2187990
Nia C <157457101+nlclark216@users.noreply.github.com>
2024-11-21 15:46:03 -0500

    login complete

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,6 +7,7 @@ import './index.css';
 import { Provider } from 'react-redux';
 import configureStore from './store';
 import { restoreCSRF, csrfFetch } from './store/csrf';
+import * as sessionActions from './store/session';
 
 const store = configureStore();
 
@@ -15,6 +16,7 @@ if (import.meta.env.MODE !== 'production') {
 
   window.csrfFetch = csrfFetch;
   window.store = store;
+  window.sessionActions = sessionActions;
 }
 
 if (process.env.NODE_ENV !== 'production') {
---
+++ frontend/src/store/session.js
@@ -0,0 +1,54 @@
+import { csrfFetch } from "./csrf";
+
+// action creators
+const SET_USER = 'session/setUser';
+const REMOVE_USER = 'session/removeUser';
+
+const setUser = (user) => {
+    return {
+        type: SET_USER,
+        payload: user
+    }
+};
+
+const removeUser = (user) => {
+    return {
+        type: REMOVE_USER,
+        payload: user
+    }
+};
+
+// thunk action
+export const login = (user) => async (dispatch) => {
+    // get user credentials
+    const { credential, password } = user;
+
+    const res = await csrfFetch('/api/session', {
+        method: 'POST',
+        body: JSON.stringify({
+            credential,
+            password
+        })
+    })
+
+    const data = await res.json();
+    dispatch(setUser(data.user));
+    return res;
+};
+
+// initial state - user is logged out
+
+const initialState = { user: null };
+
+// reducer
+
+export default function sessionReducer(state = initialState, action) {
+    switch(action.type){
+        case SET_USER:
+            return {...state, user: action.payload};
+        case REMOVE_USER:
+            return {...state, user: null};
+        default:
+            return state;
+    }
+};
