84181ece8c1805b3aafb90edb77a78d9043e23ac
Nia C <157457101+nlclark216@users.noreply.github.com>
2024-11-21 18:53:03 -0500

    profile menu opens and closes

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -1,26 +1,16 @@
 // frontend/src/components/Navigation/Navigation.jsx
 import { NavLink } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import ProfileButton from "./ProfileButton";
-import * as sessionActions from '../../store/session';
 import './Navigation.css';
 
 export default function Navigation({isLoaded}) {
     const sessionUser = useSelector(state => state.session.user);
-    const dispatch = useDispatch();
-
-    const logout = e => {
-        e.preventDefault();
-        dispatch(sessionActions.logout());
-    }
 
     const sessionLinks = sessionUser ? (
         <>
             <li>
                 <ProfileButton user={sessionUser} />
-            </li>
-            <li>
-                <button onClick={logout} >Log Out</button>
             </li>
         </>
     ) : (
--- frontend/src/components/Navigation/ProfileButton.jsx
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -1,10 +1,39 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
 import { CgProfile } from "react-icons/cg";
+import * as sessionActions from '../../store/session';
+import './ProfileButton.css';
 
 
-export default function ProfileButton() {
+export default function ProfileButton({ user }) {
+    const dispatch = useDispatch();
+    const [showMenu, setShowMenu] = useState(false);
+
+    const toggleMenu = () => {
+        if(showMenu === false) setShowMenu(true)
+            else setShowMenu(false)
+    }
+
+    const logout = (e) => {
+        e.preventDefault();
+        dispatch(sessionActions.logout());
+    };
+
+    const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
+
     return (
-        <div style={{width:'300px', height:'300px'}}>
+        <>
+          <button onClick={() => toggleMenu()}>
             <CgProfile />
-        </div>
-    );
+          </button>
+          <ul className={ulClassName}>
+            <li>{user.username}</li>
+            <li>{user.firstName} {user.lastName}</li>
+            <li>{user.email}</li>
+            <li>
+              <button onClick={logout}>Log Out</button>
+            </li>
+          </ul>
+        </>
+      );
 } 
