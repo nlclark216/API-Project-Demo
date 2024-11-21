6a143cef5590ae95191a08b96ee2787ed9f92634
Nia C <157457101+nlclark216@users.noreply.github.com>
2024-11-21 01:41:21 -0500

    initial frontend setup complete

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,17 @@
+// frontend/src/main.jsx
+
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
+import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import { Provider } from 'react-redux';
 import configureStore from './store';
 
 const store = configureStore();
+
+if (process.env.NODE_ENV !== 'production') {
+  window.store = store;
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
