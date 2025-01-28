a6987e106c30bf87a42d4daacaa7d8f57c488f62
Nia C <157457101+nlclark216@users.noreply.github.com>
2025-01-28 00:05:50 -0500

    state abbreiations added

--- frontend/src/components/Spots/Spots.jsx
+++ frontend/src/components/Spots/Spots.jsx
@@ -4,6 +4,7 @@ import * as spotActions from '../../store/spots';
 import { FaStar } from "react-icons/fa";
 import './Spots.css';
 import { Tooltip } from 'react-tooltip';
+import StateAbbObj from "../StateAbbr/StateAbbr";
 
 import { Link } from "react-router-dom";
 
@@ -24,24 +25,24 @@ export default function AllSpots() {
         <div className="spot-tiles">
         {spotArr && spotArr.map(spot=>(
             <div
-             key={spot.id}
+             key={spot?.id}
              data-tooltip-class-name="img-info"
              data-tooltip-id="tooltip"
              data-tooltip-float={true}
              data-tooltip-place="bottom"
-             data-tooltip-content={spot.name}
+             data-tooltip-content={spot?.name}
              >
-                <Link to={`/spots/${spot.id}`} >
+                <Link to={`/spots/${spot?.id}`} >
                     <div className="preview-img">
                         <Tooltip id="tooltip" followCursor/>
-                            <img src={spot.previewImage} height='300px' width='300px' alt='Preview image of location' />
+                            <img src={spot?.previewImage} height='300px' width='300px' alt='Preview image of location' />
                     </div>
                     <div className="spot-info">
-                        <span>{`${spot.city}, ${spot.state}`}</span>
-                        <span className="rating"><FaStar className="star" id="home" />{spot.avgRating ? spot.avgRating.toFixed(1) : "New"}</span>
+                        <span>{spot?.city}, <StateAbbObj state={spot?.state} /></span>
+                        <span className="rating"><FaStar className="star" id="home" />{spot?.avgRating ? spot?.avgRating.toFixed(1) : "New"}</span>
                     </div>
                     <div>
-                        <span className="price"><b>${spot.price}</b>night</span>
+                        <span className="price"><b>${spot?.price}</b>night</span>
                     </div>
                 </Link>
             </div>
